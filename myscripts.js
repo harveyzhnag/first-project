6fbdf949ae014d33b41d53659a63c1f7c4687cbf
unknown <harveyzhang500@gmail.com>
2018-01-27 14:39:22 -0800

    javascript

---
+++ myscripts.js
@@ -0,0 +1,19 @@
+var u ="first mesasage";
+var z ="seccond message";
+
+
+function myfunction(num1,num2) {
+    return 1+2;
+}
+
+
+myfunction(5, 7);
+
+alert("k");
+
+console.log(z)
+
+function changerheader(){
+  heading = document.querySelector("h1");
+  heading.innerHTML="first message";
+}
