22af463af5d0fe8bf59f2a78fcf4ea2d97179c95
unknown <harveyzhang500@gmail.com>
2018-02-03 14:56:21 -0800

    button scripts div

---
+++ scripts.js
@@ -0,0 +1,11 @@
+console.log("i'm working");
+
+alert("i'm working");
+
+
+
+function myFunction() {
+    var btn = document.createElement("BUTTON")("h2");
+    var t   =document.createTextNode("CLICK ME")
+    document.body.appendChild(btn);
+}
